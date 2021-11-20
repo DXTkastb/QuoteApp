55a9ebf0aaa1f3c51c9fecc5352d91e8dbf94bc0
DXTkastb <dxtkastb@gmail.com>
2021-11-20 17:01:07 +0530

    added viewresolver

--- src/com/app/config/AppConfiguration.java
+++ src/com/app/config/AppConfiguration.java
@@ -1,12 +1,31 @@
 package com.app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(value="com.app")
 public class AppConfiguration {
+	
+	
+	@Bean
+	public ViewResolver viewresolverbean() {
+		
+		UrlBasedViewResolver urlbasedviewresolver=new UrlBasedViewResolver();
+		
+		
+		urlbasedviewresolver.setViewClass(JstlView.class);
+		urlbasedviewresolver.setPrefix("WEB-INF/views");
+		urlbasedviewresolver.setSuffix(".jsp");
+				
+		return urlbasedviewresolver;
+		
+	}
 
 }
